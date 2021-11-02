1dcdf49ff321b6e7289bf696fbb9ec0a04f806ca
Breanne <BreMarotta@gmail.com>
2021-11-02 10:02:05 -0400

    commit tree 4b316a594d1a15341f06171a0e7b6617e025d535

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let multiply= 31*2;
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+
+let random= Math.floor(Math.random()*1)+1;
+
+
+let num3= 24;
+let num4= 5;
+let mod= num3 % num4;
+
+
+let max= Math.max(1, 2, 13, 20);
